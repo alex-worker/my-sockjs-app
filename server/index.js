301e63022e0a50fca0b441f0665cde34cfb911c5
alex-worker <43813540+alex-worker@users.noreply.github.com>
2018-10-05 16:10:11 +0300

    move server code to server.js file

--- server/index.js
+++ server/index.js
@@ -1,85 +1,9 @@
 'use strict';
 
-const json_schema= require('../common/schema/webchat-srv.json')
 const http = require('http')
-
-const server = http.createServer()
-server.listen(9999, '0.0.0.0')
+const http_server = http.createServer()
+http_server.listen(9999, '0.0.0.0')
 console.log(' [*] Listening on 0.0.0.0:9999')
 
-const Ajv = require('ajv')
-const ajv = new Ajv({
-	allErrors: true,
-	verbose:   true,
-})
-
-const validate = ajv.compile(json_schema)
-
-function whisper (id, message) {
-	if ( !clients[id] ) return;
-
-	clients[id].write( JSON.stringify(message) );
-}
-
-function broadcast (message, exclude) {
-	for ( var i in clients ) {
-		if ( i != exclude ) clients[i].write( JSON.stringify(message) );
-	}
-}
-
-// const my_server = new Server()
-// my_server.install( server )
-let onConnection = (conn) => {
-  clients[conn.id] = conn;
-  broadcast({ type: 'newUser' }, conn.id);
-  whisper(conn.id, { type: 'history', message: buffer, id: conn.id });
-
-	conn.on('data', function onDataCB (data) {
-		try {
-			data = JSON.parse(data);
-		}
-		catch(err){
-			// console.error( err )
-			// clients[conn.id].write( JSON.stringify(message) );
-			whisper(conn.id, { type: 'error', message: 'error!', id: conn.id });
-			return
-		}
-		
-		var valid = validate(data)
-		if (valid) {
-			// console.log('Valid!');
-		}
-		else {
-			console.log( data );
-			console.log('Invalid: ' + ajv.errorsText(validate.errors));
-			whisper(conn.id, { type: 'error', message: ajv.errorsText(validate.errors), id: conn.id });
-			return
-		}
-
-		if ( data.type == 'text' ) {
-			if ( !data.message ) return;
-
-			data.message = data.message.substr(0, 128);
-
-			if ( buffer.length > 15 ) buffer.shift();
-			buffer.push(data.message);
-
-			broadcast({ type: 'message', message: data.message, id: conn.id });
-		}
-
-		// TODO: add user name
-	});
-
-	conn.on('close', function onCloseCB () {
-		delete clients[conn.id];
-		broadcast({ type: 'userLeft' });
-	});
-}
-
-var clients = {};
-var buffer = [];
-
-const sockjs = require('sockjs');
-const my_sockjs = sockjs.createServer()
-my_sockjs.installHandlers(server, {prefix:'/chat'});
-my_sockjs.on('connection', onConnection )
+const Server = require('./server')
+Server.install( http_server )
--- server/server.js
+++ server/server.js
@@ -1,23 +1,84 @@
 'use strict';
 
-const sockjs = require('sockjs');
+const sockjs = require('sockjs')
+const Ajv = require('ajv')
+const ajv = new Ajv({
+	allErrors: true,
+	verbose:   true,
+})
+const json_schema= require('../common/schema/webchat-srv.json')
+const validate = ajv.compile(json_schema)
 
-class Server {
+let clients = {}
+let buffer = []
 
-    onConnection(conn){
-        conn.on('data', function(message) {
-            conn.write(message);
-        })
+function whisper (id, message) {
+	if ( !clients[id] ) return
+	clients[id].write( JSON.stringify(message) )
+}
+
+function broadcast (message, exclude) {
+	for ( var i in clients ) {
+		if ( i != exclude ) clients[i].write( JSON.stringify(message) )
+	}
+}
+
+let onData = (id,data) => {
+
+    try {
+        data = JSON.parse(data)
+    }
+    catch(err){
+        console.error( err )
+        return
+    }
+    // console.log( id + ' : ' + JSON.stringify(data) )
+
+    var valid = validate(data)
+    if (valid) {
+        console.log('Valid!');
+    }
+    else {
+        console.log( data );
+        console.log('Invalid: ' + ajv.errorsText(validate.errors));
+        whisper(id, { type: 'error', message: ajv.errorsText(validate.errors), id: id });
+        return
     }
 
-    install(http_server){
+    if ( data.type == 'text' ) {
+        if ( !data.message ) return;
 
-        this.sockjs = sockjs.createServer()
-        this.sockjs.installHandlers(http_server, {prefix:'/chat'});
-        this.sockjs.on('connection', this.onConnection )
+        data.message = data.message.substr(0, 128);
 
+        if ( buffer.length > 15 ) buffer.shift();
+        buffer.push(data.message);
+
+        broadcast({ type: 'message', message: data.message, id: id });
     }
 
 }
 
-module.exports = Server
+let onClose = (id) => {
+    delete clients[id];
+    broadcast({ type: 'userLeft' });
+}
+
+let onConnection = (conn) => {
+    clients[conn.id] = conn
+    broadcast({ type: 'newUser' }, conn.id)
+    whisper(conn.id, { type: 'history', message: buffer, id: conn.id })
+    conn.on('data', (data) => { onData(conn.id, data )} )
+	conn.on('close', () => { onClose(conn.id )} );
+}
+
+module.exports = {
+
+    install: function(http_server) {
+
+        let my_sockjs = sockjs.createServer()
+        my_sockjs.installHandlers(http_server, {prefix:'/chat'})
+        my_sockjs.on('connection', onConnection )
+
+    }
+
+}
