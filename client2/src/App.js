865c325235f6d7fb58de98cb7bd1bc90380e0382
alex-worker <43813540+alex-worker@users.noreply.github.com>
2018-10-30 19:49:27 +0300

    try some custom hook

--- client2/src/App.js
+++ client2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useState, useReducer } from 'react'
 import './App.scss'
 import {Header} from './layouts/Header'
 import {Footer} from './layouts/Footer'
@@ -6,6 +6,7 @@ import {Login} from './pages/Login'
 import {Loading} from './pages/Loading'
 
 import Client from './client'
+import { useMyCore } from './useMyCore';
 
 // import { useMyCore } from "./useMyCore";
 
@@ -21,56 +22,69 @@ const initialState = {
 
 var client
 
-const conf = {
-  "protocol": "http",
-    "ip": "0.0.0.0",
-    "port": 9999,
-    "bound": "chat"
-}
+// const conf = {
+//   "protocol": "http",
+//     "ip": "0.0.0.0",
+//     "port": 9999,
+//     "bound": "chat"
+// }
 
-function processClient( mess ){
-  // if ( this.callbackArray[mess.type] === undefined ) {
-    console.log( mess )
-  }
+// function processClient( mess ){
+//   // if ( this.callbackArray[mess.type] === undefined ) {
+//     console.log( mess )
+//     App.setError('ohoho')
+//   }
 
-const doLogin = (state,action) => {
-  // setPage('Loading')
-  client = new Client( conf.protocol+"://"+conf.ip+':'+conf.port+'/'+conf.bound, processClient );
-  client.connect();
-  return { ...state, page:'Loading', login: action.name};
-}
+// const doLogin = (state,action) => {
+//   // setPage('Loading')
+//   client = new Client( conf.protocol+"://"+conf.ip+':'+conf.port+'/'+conf.bound, processClient );
+//   client.connect();
+//   return { ...state, page:'Loading', login: action.name};
+// }
 
-const reducer = (state,action) => {
-  // console.log( state )
-  // console.log( action )
-  switch (action.type) {
-    case 'login': return doLogin(state,action);
-    default:
-      console.log( 'unsupported action:'+action)
-      return state
-  }
-}
+// const reducer = (state,action) => {
+//   // console.log( state )
+//   // console.log( action )
+//   switch (action.type) {
+//     case 'login': return doLogin(state,action);
+//     default:
+//       console.log( 'unsupported action:'+action)
+//       return state
+//   }
+// }
+
+// const myFunc = () => {
+  // return [{image:'asdasd', count:10 },50]
+// }
 
 const App = () => {
 
-  // const [error,setError] = useState( '' )
-  // const [login,setLogin] = useState( default_user )
-  // const [page,setPage] = useState( default_page )
-  const [state, dispatch] = useReducer(reducer, initialState);
+  // let[{image:cnt,count}] = myFunc();
+  // console.log( cnt )
+  // console.log( count )
+  // console.log( my_calue )
+
+  const [error,setError] = useState( '' )
+  const [login,setLogin] = useState( default_user )
+  const [page,setPage] = useState( default_page )
+  const [state] = useMyCore(login)
+  // console.log( state )
+  // const [state, dispatch] = useReducer(reducer, initialState);
   
   const getCurrentPage = ( page ) => {
     if ( page === 'Login')
-      return <Login username={state.login} onSubmit={ (name)=>{ dispatch({type:'login',name:name}) } } />
-    if ( page === 'Login')
+      return <Login username={login} onSubmit={ setLogin } />
+    if ( page === 'Loading')
       return <Loading />
     return 'unsupported '+page
   }
 
-  // console.log( some_data )
+  console.log( 'app' )
+
   return (
   <>
-    <Header />{state.login} {state.page} {state.error}
-    <div className="central">{getCurrentPage(state.page)}</div>
+    <Header />{login} {page} {error}
+    <div className="central">{getCurrentPage(page)}</div>
     <Footer />
   </>
   )
--- client2/src/useMyCore.js
+++ client2/src/useMyCore.js
@@ -1,10 +1,22 @@
 import { useState, useEffect } from "react";
 
 export let useMyCore = (params) => {
-    // let [state, setState] = useState({});
+    let [state, setState] = useState({my_test:'sss'});
+    
+    let myEffectOff = () => {
+        console.log( 'effect off')
+    }
+
+    let myEffectOn = () => {
+        console.log( 'effect on')
+        return myEffectOff
+    }
+
+    useEffect( myEffectOn, params );
+
     // // return [state]
     // console.log( 'useMyCore' );
     // console.log( params );
 
-    // return [state]
+    return [state];
 }
