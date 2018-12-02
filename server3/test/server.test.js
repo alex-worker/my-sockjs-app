0e00730fd2961391da6b0bf477d4d066c73dbf16
alex-worker <43813540+alex-worker@users.noreply.github.com>
2018-12-02 19:10:54 +0300

    подходы к тестированию sockjs клиента через promise

--- server3/test/server.test.js
+++ server3/test/server.test.js
@@ -8,171 +8,119 @@ const http = require('http')
 const supertest = require('supertest')
 const Server = require('../server')
 
-let name
-// let http_server
-// let app
+const SockJS = require('sockjs-client')
 
-// const test_illegal_use = async (done) => {
+const connect_url = conf.protocol+"://"+conf.ip+':'+conf.port+'/'+conf.bound
+
+let common_name
+let app // for app.use( ... )
+let request // for [await] request( ... ).
+
+// let client // for chat-client request
+
+function promise_sockjs(){
+    return new Promise(function(resolve, reject) {
+
+        // console.log( connect_url )
+        let client = new SockJS( connect_url )
+
+        client.onerror = (err) => { 
+            reject(err)
+        }
+
+        client.onclose = (err) => {
+            reject(err)
+        }
+
+        client.onopen = function() {
+            resolve(client)
+        }
+        
+        // client.onmessage = function() {
+        //     resolve(client)
+        // }
+
+    })
+}
+
+const test_illegal_use = async () => {
+
+    let uid = Server.addUser( common_name )
+    assert.notEqual( uid, false )
+    assert.notEqual( uid, undefined )
+    assert.notEqual( uid, null )
+
+    let client = await promise_sockjs()
+    await client.close()
+    // await promise_sockjs()
+
+    // return request
+    // .get('/api/v1')
+    // .set('Accept', 'application/json')
+    // .expect("Content-type",'text/plain')
+    // .expect(200)
     
-//     // let uid = Server.addUser( name )
-//     // assert.notEqual( uid, false )
-//     // assert.notEqual( uid, undefined )
-//     // assert.notEqual( uid, null )
-//     http_server = http.createServer()
+}
 
-//     request(http_server)
-//     .get('/')
-//     .expect(200, done);
+const test_normal_use = async () => {
 
-//     // let client_http = http.request({
-//     //         host: conf.ip,
-//     //         port: conf.port,
-//     //         path: '/'
-//     //       }
-//     // )
+    let uid = Server.addUser( common_name )
+    assert.notEqual( uid, false )
+    assert.notEqual( uid, undefined )
+    assert.notEqual( uid, null )
 
-// }
-
-// const test_normal_use = () => {
-//     let uid = Server.addUser( name )
-//     assert.notEqual( uid, false )
-//     assert.notEqual( uid, undefined )
-//     assert.notEqual( uid, null )
-// }
-
-// const server_test = async () => {
-
-//     before( function() {
-//         name = faker.name.firstName()
-//     })
-//     beforeEach( async () => {
-//         // http_server = http.Server()
-//         // await http_server.listen(conf.port, conf.ip)
-//         // Server.install( http_server, conf.bound  )
-//     })
-//     afterEach( async () => {
-//         await http_server.close()
-//     })
-
-//     describe("server normal use", async function() {
-//         // it( 'test normal use', test_normal_use )
-//     })
-//     describe("server illegal use", async function() {
-//         it( 'test illegal use', test_illegal_use )
-//     } )
-
-// }
-
-// describe("Server", server_test )
-
-// let app
-// let server
-// let request
-
-// app = http.createServer( (req, res) => {
-//         res.writeHead(200, {'Content-Type': 'text/plain'});
-//         res.write('Hello World!');
-//         res.end();
-//         }
-// )
-
-// server = app.listen(conf.port)
-// request = supertest(app)
-
+    return request
+        .get('/api/v1/laps')
+        .set('Accept', 'application/json')
+        .expect("Content-type",'text/plain')
+        .expect(200)
+    }
 
 describe("Server", function() {
 
-    // let http_server
-    let app
-    let server
-    let request
+    let server // server.close() after each test
 
-    before(function(done){
+    before( function() {
+        // делаем имя одинаковое для всех тестов для проверки
+        // что данные не сохраняются от предыдущего теста
+        common_name = faker.name.firstName() 
+    })
+
+    beforeEach( (done) => {
         app = connect()
-
-        // http_server = http.createServer( (req, res) => {
-        //     res.writeHead(200, {'Content-Type': 'text/plain'});
-        //     res.write('Hello World!');
-        //     res.end();
-        //     })
-
         server = http.createServer(app).listen(conf.port, conf.ip)
-        // server = http_server.listen(conf.port, conf.ip)
-    //     // request = supertest.agent('http://'+conf.ip+':'+conf.port)
+        Server.install( server, conf.bound  )
+
         request = supertest(server)
+
+        app.use( (req, res, next) => {
+            res.writeHead(200, {'Content-Type': 'text/plain'})
+            res.write('Look at you, hacker!')
+            res.end()
+            next()
+        })
+
+        // const connect_url = conf.protocol+"://"+conf.ip+'::'+conf.port+'/'+conf.bound
+        // client =  new SockJS( connect_url )
+
         done()
-      })
+    })
     
-    after(async function(){
-        // await request.close()
+    afterEach(async function(){
         await server.close()
-        // console.log('after')
-      })
+    })
 
-    // it.only("should return home page",function(){
-    //     return request
-    // })
+    it('test http server', async () => {
     
-    it('test server', async () => {
-
-        // server.use(async function (ctx) {
-        //     const filename = path.join(__dirname, 'fixtures/sample.html')
-        //     const html = fs.readFileSync(filename, {encoding: 'utf8'})
-        //     ctx.body = await ctx.pdf.generateFromHtml(html)
-        // })
-
-        // return request.get('/api/v1/laps/85299')
-        // .expect(200, { data: {} })
-
         return request
         .get('/api/v1/laps/85299')
-        // .expect("Content-type",/json/)
+        .set('Accept', 'application/json')
+        .expect("Content-type",'text/plain')
         .expect(200)
-        // .end(function(err,res){
-        //     console.log( res )
-        //     done()
-        // })
 
     })
 
+    it('test normal use', test_normal_use )
+    it('test illegal use', test_illegal_use )
+
 })
-
-        // done()
-
-        // .get('/api/v1/laps/85299')
-        // .end( (err, res) => {
-        //     console.log( JSON.stringify(res.body) )
-        //     console.log( JSON.stringify(err.body) )
-        //     done()
-        // })
-        // .then( res => {
-        //     console.log( JSON.stringify(res.body) )
-        //     done()
-        // })
-        // .catch(err => {
-        // //     // err.message, err.response
-        //     console.log( err )
-        //     done()
-        //  });
-
-    // })
-
-
-    // afterEach( async function(done) {
-    //     http_server.close()
-    //     done()
-    // })
-
-    // it('test server', async (done) => {
-    //     http_server = http.createServer()
-    //     http_server.listen(done)
-
-    //     request(http_server)
-    //     .get('/')
-    //     .expect(200, () => {
-    //         http_server.close()
-    //         done()
-    //       })
-    //     // return done();
-    // })
-// })
